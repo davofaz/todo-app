b1d93fc6f92f10c5a816a3685da833d8678b59ce
davofaz <dfazaldin@gmail.com>
2023-06-05 14:57:48 +0100

    created todo app via help from chatgpt using typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { FiTrash } from 'react-icons/fi';
+import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+let myuuid = uuidv4();
+
+
+
+type Todo = {
+    id: string;
+    text: string;
+    completed: boolean;
+};
+
+const App: React.FC = () => {
+    const [todos, setTodos] = useState<Todo[]>([]);
+  const [newTodo, setNewTodo] = useState('');
+
+    const addTodo = () => {
+        if (newTodo.trim() === '') return;
+
+
+        const todo: Todo = {
+            id: uuidv4(),
+            text: newTodo,
+            completed: false,
+        };
+
+        setTodos([...todos, todo]);
+        setNewTodo('');
+    }
+
+    const deleteTodo = (id: string) => {
+        setTodos(todos.filter((todo) => todo.id !== id))
+    }
+
+    const toggleTodoCompletion = (id: string) => {
+        setTodos(
+            todos.map((todo) => {
+                if (todo.id === id) {
+                    return {
+                        ...todo,
+                        completed: !todo.completed
+                    }
+                }
+                return todo
+            })
+        )
+    }
+
+    return (
+        <div className="App">
+            <h1>Todo App</h1>
+            <div className="todo-container">
+                <input
+                    type="text"
+                    placeholder="Add a new todo"
+                    value={newTodo}
+                    onChange={(e) => setNewTodo(e.target.value)}
+                />
+                <button onClick={addTodo}>Add</button>
+            </div>
+            <ul className="todo-list">
+                {todos.map((todo) => (
+                    <li key={todo.id}>
+                        <span
+                            className={`todo-text ${todo.completed ? 'completed' : ''}`}
+                            onClick={() => toggleTodoCompletion(todo.id)}   
+
+                        >
+                            {todo.text}
+                        </span>
+                        <FiTrash className="delete-icon" onClick={() => deleteTodo(todo.id)} />
+                    </li>       
+                ))}
+            </ul>
+        </div>
+    )
 }
 
 export default App;
