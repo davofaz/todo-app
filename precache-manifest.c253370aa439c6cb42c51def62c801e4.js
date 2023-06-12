bbf3c6b00edf17b0d55452d60c3321be8442bb45
davofaz <dfazaldin@gmail.com>
2023-06-12 14:37:17 +0100

    Updates

---
+++ precache-manifest.c253370aa439c6cb42c51def62c801e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "94290405b9194e8d19d5",
+    "url": "/todo-app/static/css/main.b612d616.chunk.css"
+  },
+  {
+    "revision": "94290405b9194e8d19d5",
+    "url": "/todo-app/static/js/main.c3a0364e.chunk.js"
+  },
+  {
+    "revision": "f7728c30f6339eef41ed",
+    "url": "/todo-app/static/js/runtime~main.eca9ff85.js"
+  },
+  {
+    "revision": "4080ae00aff39bade4f0",
+    "url": "/todo-app/static/js/2.b43dd384.chunk.js"
+  },
+  {
+    "revision": "e90d7bfed146cd78e73b5dd2b8dcafd5",
+    "url": "/todo-app/static/media/header-background-image.e90d7bfe.jpg"
+  },
+  {
+    "revision": "7a5be7b9858f0388b0441b0fc57db7f6",
+    "url": "/todo-app/index.html"
+  }
+];
