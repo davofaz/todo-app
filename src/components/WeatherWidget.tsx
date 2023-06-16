fbc172fe55030a840b9cc4382272432fb63ce984
davofaz <dfazaldin@gmail.com>
2023-06-16 16:17:32 +0100

    updated index,app.css, weatherwidget and index.css to make the weather to toggle c or f on click

--- src/components/WeatherWidget.tsx
+++ src/components/WeatherWidget.tsx
@@ -1,55 +1,88 @@
 import React, { useState, useEffect } from 'react';
 
 const WeatherWidget = () => {
-    const [temperature, setTemperature] = useState(null);
-    const [desc, setDesc] = useState(null);
-    const [minTemp, setMinTemp] = useState(null);
-    const [maxTemp, setMaxTemp] = useState(null);
+    const [temperature, setTemperature] = useState<number | null>(null);
+    const [desc, setDesc] = useState<number | null>(null);
+    const [minTemp, setMinTemp] = useState<number | null>(null);
+    const [maxTemp, setMaxTemp] = useState<number | null>(null);
     const [icon, setIcon] = useState(undefined);
+    const [isCelsius, setIsCelsius] = useState(true);
+    const [error, setError] = useState<Error | null>(null);
 
     useEffect(() => {
         const fetchWeatherData = async () => {
             try {
                 const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
-                console.log(apiKey);
-                navigator.geolocation.getCurrentPosition(async (posititon) => {
-                    const { latitude, longitude } = posititon.coords;
+                navigator.geolocation.getCurrentPosition(async (position) => {
+                    const { latitude, longitude } = position.coords;
                 
                     const response = await fetch(
-                        `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${latitude},${longitude}&days=1`
+                       `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${latitude},${longitude}&days=1`
                     );
+
+                    // Simulate an error by checking the response status
+                    if (!response.ok) {
+                        throw new Error('Failed to fetch weather data');
+                    }
+
                     const data = await response.json();
-                    const temp = data && data.current && data.current.temp_c;
-                    const minTemp = data && data.forecast && data.forecast.forecastday[0].day.mintemp_c;
-                    const maxTemp = data && data.forecast && data.forecast.forecastday[0].day.maxtemp_c;
+                    const temp = isCelsius ? data.current.temp_c : data.current.temp_f;
+                    const minTemp = isCelsius ? data.forecast.forecastday[0].day.mintemp_c : data.forecast.forecastday[0].day.mintemp_f;
+                    const maxTemp = isCelsius ? data.forecast.forecastday[0].day.mintemp_c : data.forecast.forecastday[0].day.mintemp_f;
                     const desc = data && data.current && data.current.condition.text;
                     const icon = data && data.current && data.current.condition.icon;
+                    
 
-                setTemperature(temp);
-                setDesc(desc);
-                setIcon(icon);
-                setMinTemp(minTemp);
-                setMaxTemp(maxTemp);
+                    setTemperature(temp);
+                    setDesc(desc);
+                    setIcon(icon);
+                    setMinTemp(minTemp);
+                    setMaxTemp(maxTemp);
+
+
+
                 });
             } catch (error) {
                 console.error('Error fetching weather data:', error);
+                setError(error as Error);
             }
         };
 
         fetchWeatherData();
-    }, []);
+    }, [isCelsius]);
+
+    const toggleTempScale = () => {
+        setIsCelsius((prevIsCelsius) => !prevIsCelsius);
+    }
+
+    const renderTemp = (value: number | null) => `${value} ${isCelsius ? '\u00B0C' : '\u00B0F'}`
 
     return (
-        <div className="weather-widget">
-            {temperature && (
-                <div>
-                    <div><img src={icon} alt="Weather Icon" height="32" /></div>
-                    <div>{temperature} &#8451; {desc}</div>
-                    <div><small>Min: {minTemp} &#8451; | Max: {maxTemp} &#8451;</small></div>
-                </div>
-             )}
+        <div className="weather-widget" onClick={toggleTempScale}>
+            {error ? (
+                <div>Error: {error.message}</div>
+            ) : (
+                <>
+                    {temperature && (
+                        <div>
+                            <div>
+                                <img src={icon} alt="Weather Icon" height="32" />
+                            </div>
+                            <div>
+                                {renderTemp(temperature)} {desc}
+                            </div>
+                            <div>
+                                <small>
+                                    Min: {renderTemp(minTemp)} | Max: {renderTemp(maxTemp)}
+                                </small>
+                            </div>
+                        </div>
+                    )}
+                </>
+            )}
         </div>
     );
+
 };
 
 export default WeatherWidget;
