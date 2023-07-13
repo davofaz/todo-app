8960abbc945995d86c2d51faba5a9c83d4227075
davofaz <dfazaldin@gmail.com>
2023-07-13 21:49:18 +0100

    feat: new todos added to top of the list

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,7 @@ const App: React.FC = () => {
             backgroundColor: ''
         };
 
-        setTodos((prevTodos) => [...prevTodos, todo]);
+        setTodos((prevTodos) => [todo, ...prevTodos]);
         setNewTodo('');
     };
 
